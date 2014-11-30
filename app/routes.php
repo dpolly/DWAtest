3722e3ad19783ae3e4782fc89dd50b36087c3189
Denise Polly <dpolly@g.harvard.edu>
2014-11-30 09:54:35 -0600

    Debug sections for initial page design, environment and database setup.

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,8 @@
 <?php
-
+/*
+ * DMP 11-28-2014: added Debug Routes for testing page design: Master
+ * DMP 11-30-2014: added Debug Routes for validations: DB Connectivity, Environment, Error Checking
+ */
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,7 +14,125 @@
 |
 */
 
-Route::get('/', function()
+/**
+ * DEBUG Section **********************************************
+ */
+/**
+ * Testing Design-Master
+ */
+Route::get('/test-master', function()
+    {
+        return View::make("master");
+    }
+);
+
+/**
+ * Validate Database Connectivity
+ */
+Route::get('mysql-test', function() {
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    #echo Pre::render($results);
+    var_dump($results);
+});
+
+/**
+ * Validate Environment Settings
+ */
+Route::get('/get-environment',function() {
+    echo "Environment: ".App::environment();
+});
+
+/**
+ * Validate Error Response
+ */
+Route::get('/trigger-error',function() {
+    # Class Foobar should not exist, so this should create an error
+    $foo = new Foobar;
+});
+
+
+
+/**
+* Index
+
+Route::get('/', 'IndexController@getIndex');
+
+/**
+ * User
+* (Explicit Routing)
+
+Route::get('/signup','UserController@getSignup' );
+Route::get('/login', 'UserController@getLogin' );
+Route::post('/signup', 'UserController@postSignup' );
+Route::post('/login', 'UserController@postLogin' );
+Route::get('/logout', 'UserController@getLogout' );
+
+/**
+ * Book
+ * (Explicit Routing)
+
+Route::get('/species', 'BookController@getIndex');
+Route::get('/species/edit/{id}', 'BookController@getEdit');
+Route::post('/species/edit', 'BookController@postEdit');
+Route::get('/species/create', 'BookController@getCreate');
+Route::post('/species/create', 'BookController@postCreate');
+Route::post('/species/delete', 'BookController@postDelete');
+
+ ## Ajax examples
+Route::get('/species/search', 'BookController@getSearch');
+Route::post('/species/search', 'BookController@postSearch');
+
+
+/**
+* Debug
+* (Implicit Routing)
+
+Route::controller('debug', 'DebugController');
+
+
+/**
+* Tag
+* (Implicit RESTful Routing)
+
+Route::resource('tag', 'TagController');
+
+
+/**
+* Demos
+* (Explicit Routing)
+
+Route::get('/demo/csrf-example', 'DemoController@csrf');
+Route::get('/demo/collections', 'DemoController@collections');
+Route::get('/demo/js-vars', 'DemoController@jsVars');
+
+Route::get('/demo/crud-create', 'DemoController@crudCreate');
+Route::get('/demo/crud-read', 'DemoController@crudRead');
+Route::get('/demo/crud-update', 'DemoController@crudUpdate');
+Route::get('/demo/crud-delete', 'DemoController@crudDelete');
+
+Route::get('/demo/collections', 'DemoController@collections');
+Route::get('/demo/query-without-constraints', 'DemoController@queryWithoutConstraints');
+Route::get('/demo/query-with-constraints', 'DemoController@queryWithConstraints');
+Route::get('/demo/query-responsibility', 'DemoController@queryResponsibility');
+Route::get('/demo/query-with-order', 'DemoController@queryWithOrder');
+
+Route::get('/demo/query-relationships-author', 'DemoController@queryRelationshipsAuthor');
+Route::get('/demo/query-relationships-tags', 'DemoController@queryRelationshipstags');
+Route::get('/demo/query-eager-loading-authors', 'DemoController@queryEagerLoadingAuthors');
+Route::get('/demo/query-eager-loading-tags-and-authors', 'DemoController@queryEagerLoadingTagsAndAuthors');
+
+Route::get('/demo/simple-ajax', 'DemoController@getSimpleAjax');
+Route::post('/demo/simple-ajax', 'DemoController@postSimpleAjax');
+
+
+/*Route::get('/', function()
 {
 	return View::make('hello');
 });
+*/
