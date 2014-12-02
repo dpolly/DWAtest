45c0eceb5158fff870bfdc2d1fdecfdb97eb5a26
Denise Polly <dpolly@g.harvard.edu>
2014-12-01 22:22:37 -0600

    Inital Upload of Directory

--- app/views/family_add.blade.php
+++ app/views/family_add.blade.php
@@ -3,24 +3,20 @@
  * DMP 12-01-2014: Created
  */
 
- @extends('_master')
+ @extends('master')
 
  @section('title')
- 	DWATest Add Family
+ 	Dpolly.me Add Family
  @stop
 
  @section('content')
 
- 	<h1>Add a family</h1>
+ 	<h1>Add A Family</h1>
 
  	{{ Form::open(array('url' => '/family/create')) }}
-
-
  		{{ Form::label('botanical name','Title') }}
  		{{ Form::text('botanical_name'); }}
-
  		{{ Form::submit('Add'); }}
-
  	{{ Form::close() }}
 
  @stop
