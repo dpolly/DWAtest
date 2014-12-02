e1bdf9fcf7ea76a29ced2915d925fd6e0750171b
Denise Polly <dpolly@g.harvard.edu>
2014-12-01 21:50:29 -0600

    Created

---
+++ app/config/production/database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*   DMP 12-01-2014: Created
+ */
+
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'plants',
+            'username'  => 'root',
+            'password'  => '',
+        ),
+
+    ),
+
+);
